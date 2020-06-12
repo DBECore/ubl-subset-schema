9af649269793d24802b81614e6765a278022a9b2
Mika Lammi <mika.lammi@dbecore.com>
2020-06-12 10:39:07 +0300

    Testing subset creation
    Changed the sheet urls, the copyright notice and selected Order, OrderResponse and Invoice schemas for action.  No changes in the cardinalities in the sheets.

--- subsetUBLSchemas-DBEcoreDemo.sh
+++ subsetUBLSchemas-DBEcoreDemo.sh
@@ -5,15 +5,15 @@ if [ "$3" = "" ]; then echo Missing results directory, environment, and dateZ ar
 # Configuration parameters
 
 export version=0.1
-export libGoogle=https://docs.google.com/spreadsheets/d/1bWAhvsb83PvkdGeMvFXiVVSWKCIZXsoiCMLhgUrHFzY
-export docGoogle=https://docs.google.com/spreadsheets/d/1Q_-5hKiUkshJP-3yEI00NTmIf0r5I091nYRNWxxksPQ
-export copyright="Subset copyright not asserted; Portions copyright &#169; OASIS Open"
+export libGoogle=https://docs.google.com/spreadsheets/d/1hFTCQri4vGqiuZyh98qDBJhM25DuLNDIsls70wIudK8
+export docGoogle=https://docs.google.com/spreadsheets/d/10Ak-7Gdaqkni_dQI3uGSC9s83ml382q1obePMC4yNp4
+export copyright="Subset copyright DBE Core Ltd. 2020; Portions copyright &#169; OASIS Open"
 export configDirectory=DBEcoreDemo
 export UBLversion=2.2
 export title="DBEcoreDemo subset of UBL 2.2"
 export package=DBEcoreDemo-subset-UBL-2.2
 export subsetColumn=DBEcoreDemoSubset
-export subsetDocsRegex="(^UBL-(RequestForQuotation|Quotation|Order|OrderResponse|OrderChange|OrderCancellation|ApplicationResponse)-2.2$)"
+export subsetDocsRegex="(^UBL-(Order|OrderResponse|Invoice)-2.2$)"
 
 bash subsetUBLSchemas-common.sh "$1" "$2" "$3" "$4"
 
