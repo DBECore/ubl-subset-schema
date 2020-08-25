f3b4559b574b2b3085ff39902fadf2d9239279b5
Mika Lammi <mika.lammi@dbecore.com>
2020-08-25 21:18:27 +0300

    Update subsetUBLSchemas-DBEcoreDemo.sh

--- subsetUBLSchemas-DBEcoreDemo.sh
+++ subsetUBLSchemas-DBEcoreDemo.sh
@@ -4,7 +4,7 @@ if [ "$3" = "" ]; then echo Missing results directory, environment, and dateZ ar
 
 # Configuration parameters
 
-export version=0.1
+export version=0.5
 export libGoogle=https://docs.google.com/spreadsheets/d/1kc9tkc65o-Zrg-u9y7krQcU4u1X4ADLFTx7N6sy0KSs
 export docGoogle=https://docs.google.com/spreadsheets/d/16sn4pqGF97-ef-DxJFyVo_TGzUuTopFumgoB9uuLNDw
 export copyright="Subset copyright DBE Core Ltd. 2020; Portions copyright &#169; OASIS Open"
@@ -12,7 +12,7 @@ export configDirectory=DBEcoreDemo
 export UBLversion=2.2
 export title="DBE Subset of UBL 2.2"
 export package=DBE-subset-UBL-2.2
-export subsetColumn=DBECoreSubset
+export subsetColumn=DBECore
 export subsetDocsRegex="(^UBL-(Order|OrderResponse|Invoice)-2.2$)"
 
 bash subsetUBLSchemas-common.sh "$1" "$2" "$3" "$4"
